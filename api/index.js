fb1e74e7a1f5b438b18ba9be0865e79d7204e7a9
madenon <brcgr230@gmail.com>
2024-08-21 01:04:01 +0100

    connexion de mongo db

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 import express from "express"
 import cors from "cors";
 import  "dotenv/config"
-import {connectDB} from "./config/db.js";
 const app = express()
+import mongoose from "mongoose";
+
+
+const PORT=process.env.PORT || 3000
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connexion à la base de donnée")
+}).catch((err)=>{
+    console.log("échec connexion à la base de donnée")
+})
 
 
 app.use(express.json())
 app.use(cors())
 
-const PORT=process.env.PORT || 3000
-connectDB
 
 
 app.listen(3000, ()=>{
