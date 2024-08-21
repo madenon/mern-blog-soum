cf4e0702ddb8f976c9c5abf0600c1c65397e8fb3
madenon <brcgr230@gmail.com>
2024-08-21 15:20:53 +0100

    Api de connexion du backend

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken"
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -10,12 +10,16 @@ export const signup = async (req, res, next) => {
     !email ||
     !password ||
     username === "" ||
-    email == "" ||
+    email === "" ||
     password === ""
   ) {
-    next(errorHandler(400, "Tout les champs sont requis"));
+    return next(errorHandler(400, "Tout les champs sont requis"));
+   
   }
+  if(password.length < 8){
+    return next(errorHandler(402, "Mot de passe faible , minimum 8 caractère"));
   
+  }
   
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
@@ -30,3 +34,36 @@ export const signup = async (req, res, next) => {
     next(errorHandler(400, "Error"));
   }
 };
+
+
+export const signin = async(req, res, next)=>{
+  const {email, password} = req.body
+  if(!email || !password || email==="" ||password===""){
+    return  next(errorHandler(400, "Les champs sont obligatoire pour se connecter"));
+
+  }
+
+  try {
+    const validUser = await User.findOne({email})
+    if(!validUser){
+      return next(errorHandler(404, "Utilisateur non trouvé"))
+
+    }
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400, "Mot de passe invalide"));
+      
+    }
+
+    const token = jwt.sign({id:validUser._id},process.env.Jwt_SECRET);
+    const {password:pass,...rest} = validUser._doc
+    res.status(200).cookie("access_token", token,{
+      httpOnly:true}).json(rest)
+    
+    
+  } catch (error) {
+    console.log(error)
+    return next(errorHandler(404,"Vos informations sont invalide"))
+  }
+
+}
