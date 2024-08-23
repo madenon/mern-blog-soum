199f702e0d3ab57d03bb5c87e2e77593d6159bec
madenon <brcgr230@gmail.com>
2024-08-23 16:55:27 +0100

    recuperation des users api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { test, updateUser,deletUser,signout } from "../controllers/user.controller.js"
+import { test, updateUser,deletUser,signout,getUsers } from "../controllers/user.controller.js"
 import { verifyToekn } from "../utils/verifyUser.js"
 const router = express.Router()
 
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/test', test)
 router.put('/update/:userId',verifyToekn,  updateUser)
 router.delete('/delete/:userId',verifyToekn,  deletUser)
+router.get('/getuser', verifyToekn, getUsers)
 router.post('/signout',  signout)
 
 export default router
